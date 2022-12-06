771df94a92d9419e9dc05e6b7f9700dcaff73d1b
Ben Ohno <benjaminohno@gmail.com>
2022-12-06 17:21:13 -0500

    formatting

--- Housing_Trend_Predictor.py
+++ Housing_Trend_Predictor.py
@@ -1,9 +1,88 @@
 import streamlit as st
 import pandas as pd
-from time_series_model import model_for_county, add_regressor_column
+# from time_series_model import model_for_county, add_regressor_column
 from streamlit_lottie import st_lottie
 from streamlit_lottie import st_lottie_spinner
 import requests
+from prophet import Prophet
+
+
+def model_for_county(county, data):
+    """
+
+    """
+    county_data = data.loc[data.county_name == county][['date', 'median_listing_price',
+                                                        'active_listing_count', 'median_days_on_market']]
+
+    # adding lockdowns as holidays to prevent model from trying to model anomoly data
+    lockdowns = pd.DataFrame([
+        {'holiday': 'lockdown_1', 'ds': '2020-03-21',
+            'lower_window': 0, 'ds_upper': '2020-06-06'},
+        {'holiday': 'lockdown_2', 'ds': '2020-07-09',
+            'lower_window': 0, 'ds_upper': '2020-10-27'},
+        {'holiday': 'lockdown_3', 'ds': '2021-02-13',
+            'lower_window': 0, 'ds_upper': '2021-02-17'},
+        {'holiday': 'lockdown_4', 'ds': '2021-05-28',
+            'lower_window': 0, 'ds_upper': '2021-06-10'},
+    ])
+
+    for t_col in ['ds', 'ds_upper']:
+        lockdowns[t_col] = pd.to_datetime(lockdowns[t_col])
+    lockdowns['upper_window'] = (
+        lockdowns['ds_upper'] - lockdowns['ds']).dt.days
+
+    county_data.columns = ['ds', 'y', 'active_listing_count',
+                           'median_days_on_market']
+
+    # instantiate Prophet model object with confidence interval with of 95%
+    m = Prophet(seasonality_mode='multiplicative',
+                holidays=lockdowns, interval_width=0.95)
+
+    m = Prophet(seasonality_mode='multiplicative', interval_width=0.95)
+
+    # adding additional regressor
+    m.add_regressor('active_listing_count')
+    m.add_regressor('median_days_on_market')
+
+    m.fit(county_data)
+
+    return m
+
+
+def add_regressor_column(regressor_name, county, months_to_forecast, data):
+    county_data = data.loc[data.county_name ==
+                           county][['date', regressor_name]]
+
+    # adding lockdowns as holidays to prevent model from trying to model anomoly data
+    lockdowns = pd.DataFrame([
+        {'holiday': 'lockdown_1', 'ds': '2020-03-21',
+            'lower_window': 0, 'ds_upper': '2020-06-06'},
+        {'holiday': 'lockdown_2', 'ds': '2020-07-09',
+            'lower_window': 0, 'ds_upper': '2020-10-27'},
+        {'holiday': 'lockdown_3', 'ds': '2021-02-13',
+            'lower_window': 0, 'ds_upper': '2021-02-17'},
+        {'holiday': 'lockdown_4', 'ds': '2021-05-28',
+            'lower_window': 0, 'ds_upper': '2021-06-10'},
+    ])
+
+    for t_col in ['ds', 'ds_upper']:
+        lockdowns[t_col] = pd.to_datetime(lockdowns[t_col])
+    lockdowns['upper_window'] = (
+        lockdowns['ds_upper'] - lockdowns['ds']).dt.days
+
+    county_data.columns = ["ds", "y"]
+
+    # instantiate Prophet model object with confidence interval with of 95%
+    m = Prophet(seasonality_mode='multiplicative',
+                holidays=lockdowns, interval_width=0.95)
+
+    m.fit(county_data)
+
+    future = m.make_future_dataframe(periods=12, freq='MS')
+
+    forecast = m.predict(future)
+
+    return forecast.yhat
 
 
 def load_lottieurl(url: str):
